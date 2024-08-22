6ac84cbebdc720aac7431aaadc7f068c6003c196
Hasnain Ashfaq <hasnainashfaq4411@gmail.com>
2024-08-22 21:41:39 +0100

    mallshopapps

---
+++ lib/Utils/app_constant.dart
@@ -0,0 +1,14 @@
+// Used to define values that are fixed and don't need to be reassigned or recalculated
+
+import 'dart:ui';
+
+class AppConstant {
+  // Static Mean these variables are accessable any where in the location..without creating class instances
+  static String appMainName ='MallShop';
+  static String appPoweredBy = "Powered by NKDev";
+  static const appMaincolor = Color.fromARGB(255, 255, 94, 0);
+  static const appSecondarycolor = Color.fromARGB(255, 255, 145, 0);
+  static const appTextcolor = Color.fromARGB(255, 255, 255, 255);
+  static const statusBarColor = Color.fromARGB(255, 255, 255, 255);
+
+}
